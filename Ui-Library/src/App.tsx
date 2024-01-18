f76c98318b1b4669d71a93233b75fb02d0dc0bd4
pavan-pani <pavanpk1012@gmail.com>
2024-01-18 14:54:19 +0530

    commiting all files

--- Ui-Library/src/App.tsx
+++ Ui-Library/src/App.tsx
@@ -1,16 +1,49 @@
-import { useState } from 'react'
+import { ButtonHTMLAttributes, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './index.css'
+import Buttons, { ButtonProps } from './Ui_components/Button/Buttons'
+import Input, { InputProps } from './Ui_components/Forms/input-field/Input'
+import DatePicker, { DatePickerProps } from './Ui_components/date-pickers/DatePicker'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState<string>('');
+ 
+  const handlechange = (value:any) => {
+    // Implement your resubmit logic here
+    setInputValue(() => {
+      console.log(value); // Log the current value
+      return value; // Set the new value
+    });
+    // setInputValue(value);
+    // console.log(inputValue);
+    // return "done"
+  };
 
+
+
+  const inputJson1:DatePickerProps={
+    type: 'date',
+    label: "Start Date",
+    value: inputValue,
+    disabled: true,
+    onChange: handlechange,
+    required: false,
+  }
+ 
   return (
     <>
-      <p className=' font-bold text-7xl text-lime-500'>My name is Pavan..!</p>
+      <p className='text'>My name is Pavan..!</p>
+      <div className='card'>
+        <DatePicker {...inputJson1}/>
+      </div>
+  
+      
+      
+
     </>
   )
-}
+  }
 
 export default App
