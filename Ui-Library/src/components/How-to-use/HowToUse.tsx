adca975621b6928470c5c69dd114193ecd192ff9
pavan-pani <pavanpk1012@gmail.com>
2024-01-27 01:10:26 +0530

    how to use stpes started

--- Ui-Library/src/components/How-to-use/HowToUse.tsx
+++ Ui-Library/src/components/How-to-use/HowToUse.tsx
@@ -5,11 +5,9 @@ import './howToUse-syles.scss'
 const HowToUse = () => {
   return (
     <div className='howToUse'>
-      <div className='useimage'>
-        <img className="img" src={howToUse}></img>
-      </div>
-      <div className='info-text'>Team is working on this feature, meanwhile you can checkout the code <a href='https://github.com/pavan-pani/Ui-library/tree/master/Ui-Library/src/Ui_components'>here</a></div>
-
+     <div className='step-main'>
+      <div className='step-num'>1</div>
+     </div>
     </div>
   )
 }
