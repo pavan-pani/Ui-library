fb8d660029e11b5ce8ed20718d57971f21bd455d
pavan-pani <pavanpk1012@gmail.com>
2024-01-28 22:10:50 +0530

    adding loader

---
+++ Ui-Library/src/components/loader/Loader.tsx
@@ -0,0 +1,9 @@
+import "./loader-styles.scss"
+
+const Loader = () => {
+  return (
+    <span className="loader">Load&nbsp;ng</span>
+  )
+}
+
+export default Loader
--- Ui-Library/src/main.tsx
+++ Ui-Library/src/main.tsx
@@ -1,11 +1,12 @@
 import React, { Suspense, lazy } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.scss'
+import Loader from './components/loader/Loader.tsx'
 const App = lazy(() => import('./App.tsx'))
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Suspense fallback={<h3>Loading...!</h3>}>
+    <Suspense fallback={<Loader/>}>
       <App />
     </Suspense>
   </React.StrictMode>,
