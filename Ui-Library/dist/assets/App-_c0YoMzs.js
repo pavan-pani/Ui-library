673ce69229ba73f54b8520a2598b8255da176661
pavan-pani <pavanpk1012@gmail.com>
2024-01-28 11:13:08 +0530

    updating build

--- Ui-Library/dist/assets/App-_c0YoMzs.js
+++
@@ -1,7 +0,0 @@
-import{r as e,_ as s,j as t,R as _,a as o}from"./index-6y5TQ62J.js";const a=e.lazy(()=>s(()=>import("./Home-laffOxr2.js"),__vite__mapDeps([0,1,2,3,4,5,6]))),r=e.lazy(()=>s(()=>import("./Docs-eBOceY_W.js"),__vite__mapDeps([7,1,2,4,5,8]))),n=e.lazy(()=>s(()=>import("./HowToUse-7ifdQjDF.js"),__vite__mapDeps([9,1,2,10]))),p=e.lazy(()=>s(()=>import("./Contact-ix8FNxum.js"),__vite__mapDeps([11,1,2,12])));function m(){return t.jsxs(_,{children:[t.jsx(o,{path:"/",element:t.jsx(a,{})}),t.jsx(o,{path:"/components",element:t.jsx(r,{})}),t.jsx(o,{path:"/how-to-use",element:t.jsx(n,{})}),t.jsx(o,{path:"/contact",element:t.jsx(p,{})})]})}export{m as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/Home-laffOxr2.js","assets/index-6y5TQ62J.js","assets/index-E5orc6tc.css","assets/logo-9Mube3MG.js","assets/Buttons-O6aUuwON.js","assets/Buttons-lpuKO3_N.css","assets/Home-dOlZIBvX.css","assets/Docs-eBOceY_W.js","assets/Docs-KdIE1vDg.css","assets/HowToUse-7ifdQjDF.js","assets/HowToUse-y_Ll307P.css","assets/Contact-ix8FNxum.js","assets/Contact-e2H2nL0-.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
